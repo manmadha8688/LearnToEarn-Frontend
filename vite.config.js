06d68f14f052c095733b132b2969be04bcf08257
Manmadha Jayamangala <manmadha.jayamangala@cloudfuze.com>
2026-06-23 15:08:45 +0530

    all isuues and fixes

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
     sourcemap: false,
-    minify: 'esbuild',
-    target: 'es2020',
-  },
-  esbuild: {
-    drop: ['console', 'debugger'],
   },
 })
